88c67ea89d023f31785f6fda54ca17f73addd517
Bhavin Moriya <bhavinmoriya@gmail.com>
2026-08-24 15:09:43 +0200

    Update

--- collatz_app.py
+++ collatz_app.py
@@ -24,7 +24,8 @@ def collatz_sequence(n):
         if n % 2 == 0:
             n = n // 2
         else:
-            n = 3 * n + 1
+            # n = 3 * n + 1
+            n = 3 * n - 1
         sequence.append(n)
     return sequence
 
